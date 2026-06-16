d4efec4f146c4397a496e1bcac38baa993306057
shivangpandya1515-code <shivangpandya1515@gmail.com>
2026-06-16 17:26:21 +0530

    All

---
+++ 16 June/que.js
@@ -0,0 +1,17 @@
+// Q1. Write a named function to print "Hello World".
+
+function displayHello(){
+    console.log("Hello World!");
+}
+
+displayHello();
+
+// Q4. Write a named function that prints the multiplication table of a given number.
+
+
+
+//  Q6. Write a named function that returns a random number between 1 and 100.
+
+// Q7. Write a named function that returns the sum of two numbers.
+
+// Q9. Write a named function that returns the factorial of a number.
